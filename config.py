2491047685d87dfbeac319d9f6678f70f9715d3b
Pranav Arora <97364962+ipranavarora@users.noreply.github.com>
2024-09-16 00:44:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///faqs.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
